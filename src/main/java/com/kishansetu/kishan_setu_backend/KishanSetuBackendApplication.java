a824c39cdec60223ebba1d8b0b53202f65934b03
ersurajrajput <thsurajsinghrajput@gmail.com>
2026-03-26 20:02:34 +0530

    first commit

---
+++ src/main/java/com/kishansetu/kishan_setu_backend/KishanSetuBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kishansetu.kishan_setu_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KishanSetuBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KishanSetuBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kishansetu/kishan_setu_backend/KishanSetuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kishansetu.kishan_setu_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KishanSetuBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
