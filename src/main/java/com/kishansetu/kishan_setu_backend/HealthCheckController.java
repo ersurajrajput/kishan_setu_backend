95d3081c56ce7d7b72ce11c6841050e6f9b904d7
ersurajrajput <thsurajsinghrajput@gmail.com>
2026-03-26 20:07:56 +0530

    health-check

---
+++ src/main/java/com/kishansetu/kishan_setu_backend/HealthCheckController.java
@@ -0,0 +1,15 @@
+package com.kishansetu.kishan_setu_backend;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health-check")
+public class HealthCheckController {
+    @GetMapping
+    public String HealthCheck(){
+        return "Kishan setu backend running fine";
+    }
+}
