409ddb49e26ae980406b59923bbdbd999df0abeb
ersurajrajput <thsurajsinghrajput@gmail.com>
2026-03-28 07:31:24 +0530

    coros test

--- src/main/java/com/kishansetu/kishan_setu_backend/config/CorsConfig.java
+++ src/main/java/com/kishansetu/kishan_setu_backend/config/CorsConfig.java
@@ -2,10 +2,12 @@ package com.kishansetu.kishan_setu_backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Configuration;
+
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
-@Configurationt
+@Configuration
 public class CorsConfig {
 
     @Bean
